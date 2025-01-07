e7b39a67b4a61a28bbe57d399abacd58f6196092
Kevin Pratama <143497270+syaoraaann@users.noreply.github.com>
2025-01-07 16:14:35 +0800

    booking v1

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from extensions import jwt
 from api.auth.endpoints import auth_endpoints
 from api.data_protected.endpoints import protected_endpoints
 from api.list_field.endpoints import list_field_endpoints
+from api.booking.endpoint import booking_endpoints
 from config import Config
 from static.static_file_server import static_file_server
 
@@ -24,6 +25,7 @@ jwt.init_app(app)
 # register the blueprint
 app.register_blueprint(auth_endpoints, url_prefix='/api/v1/auth')
 app.register_blueprint(list_field_endpoints, url_prefix='/api/v1/list_field')
+app.register_blueprint(booking_endpoints, url_prefix='/api/v1/booking')
 app.register_blueprint(protected_endpoints, url_prefix='/api/v1/protected')
 app.register_blueprint(static_file_server, url_prefix='/static/')
 
